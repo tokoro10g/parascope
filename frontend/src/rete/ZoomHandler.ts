e9e980e7bb7e9478dd1ce1891ca74d9dc81dd5fb
Yuichi Tadokoro <tokoro10g@tokor.org>
2026-01-18 12:06:32 -0800

    fix(frontend): fix direction of zoom by wheel events

--- frontend/src/rete/ZoomHandler.ts
+++ frontend/src/rete/ZoomHandler.ts
@@ -7,7 +7,7 @@ export class ZoomHandler extends Zoom {
 
     const normalized = normalizeWheel(e);
     const { left, top } = this.element.getBoundingClientRect();
-    const delta = normalized.spinY * this.intensity;
+    const delta = -normalized.spinY * this.intensity;
 
     const ox = (left - e.clientX) * delta;
     const oy = (top - e.clientY) * delta;
