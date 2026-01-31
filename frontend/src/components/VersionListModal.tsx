547f8492de4a1230fac2313cb69e58e878bb2baf
Teng Tokoro <tokoro10g@tokor.org>
2026-01-31 03:51:44 -0800

    chore: code format

--- frontend/src/components/VersionListModal.tsx
+++ frontend/src/components/VersionListModal.tsx
@@ -194,7 +194,7 @@ export const VersionListModal: React.FC<VersionListModalProps> = ({
           style={{
             display: 'flex',
             justifyContent: 'space-between',
-	    overflowWrap: 'anywhere',
+            overflowWrap: 'anywhere',
             alignItems: 'center',
             padding: '12px',
             borderBottom: '1px solid var(--border-light)',
@@ -281,7 +281,7 @@ export const VersionListModal: React.FC<VersionListModalProps> = ({
               style={{
                 display: 'flex',
                 justifyContent: 'space-between',
-	        overflowWrap: 'anywhere',
+                overflowWrap: 'anywhere',
                 alignItems: 'center',
                 padding: '12px',
                 borderBottom: '1px solid var(--border-light)',
